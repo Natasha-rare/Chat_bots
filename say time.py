3a23e107a2b7f522ca9bcd1300f2618cb81fd7c1
Natasha-rare <natasha_ea5@mail.ru>
2020-03-23 16:51:13 +0300

    Чат бот время

--- say time.py
+++ say time.py
@@ -8,6 +8,7 @@ def main():
     vk_session = vk_api.VkApi(
         token='d793c36d91cff1c0f93a216c5a95549a62788fe1cc65cee98728d74d41d90afd3e5a5cfbd789227b6c1e2')
     #токен к моему сообществу
+
     longpoll = VkBotLongPoll(vk_session, '193282244')
 
     for event in longpoll.listen():
