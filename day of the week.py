8adefe0784c26725eec67e33e9cbb5f63aedb3e2
Natasha-rare <natasha_ea5@mail.ru>
2020-03-25 16:07:21 +0300

    Бот дата-время

---
+++ day of the week.py
@@ -0,0 +1,34 @@
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import random
+import datetime
+
+def main():
+    vk_session = vk_api.VkApi(
+        token='d793c36d91cff1c0f93a216c5a95549a62788fe1cc65cee98728d74d41d90afd3e5a5cfbd789227b6c1e2')
+    #токен к моему сообществу
+
+    longpoll = VkBotLongPoll(vk_session, '193282244')
+
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            print(event)
+            print('Новое сообщение:')
+            print('Для меня от:', event.obj.message['from_id'])
+            print('Текст:', event.obj.message['text'])
+            vk = vk_session.get_api()
+            weekday = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+            try:
+                y, m, d = event.obj.message['text'].split('-')
+                message = f"^˵◕ω◕˵^) это был {weekday[datetime.datetime(int(y), int(m), int(d)).weekday()]}\n"
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=message,
+                                 random_id=random.randint(0, 2 ** 64))
+            except:
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=f"(×﹏×) Введи другую дату в формате YYYY-MM-DD",
+                                 random_id=random.randint(0, 2 ** 64))
+
+
+if __name__ == '__main__':
+    main()
