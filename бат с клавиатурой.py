409b4c39b0a2d96154f78ef88d437d6afe3db898
Natasha-rare <Natasha_ea5@mail.ru>
2020-05-14 08:44:01 +0300

    Бот геокодер с клавиатурой

--- бат с клавиатурой.py
+++ бат с клавиатурой.py
@@ -1,21 +1,26 @@
+import sys
+
+import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 
+vk_session = vk_api.VkApi(
+    token=TOKEN)
 
-from auth_data import TOKEN, group_id, VK_PASS, VK_USER, album_id
-
-from io import BytesIO
-# Этот класс поможет нам сделать картинку из потока байт
-
-import requests
-from PIL import Image
+longpoll = VkBotLongPoll(vk_session, GROUP_ID)
+vk = vk_session.get_api()
 
 
-def geo(toponym_to_find):
-    login, password = VK_USER, VK_PASS
-    vk_session = vk_api.VkApi(login, password)
-    vk = vk_session.get_api()
+def map_type():
+    return f"Choose type of map"
+
+
+def help():
+    return f"Type the place your want to see"
+
+
+def get_coords(toponym_to_find):
     geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
     geocoder_params = {
@@ -25,90 +30,102 @@ def geo(toponym_to_find):
 
     response = requests.get(geocoder_api_server, params=geocoder_params)
 
-    if not response:
-        # обработка ошибочной ситуации
-        pass
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        return toponym_coodrinates.split(" ")
+    else:
+        return None, None
 
-    # Преобразуем ответ в json-объект
-    json_response = response.json()
-    # Получаем первый топоним из ответа геокодера.
-    toponym = json_response["response"]["GeoObjectCollection"][
-        "featureMember"][0]["GeoObject"]
-    # Координаты центра топонима:
-    toponym_coodrinates = toponym["Point"]["pos"]
-    # Долгота и широта:
-    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
-    delta = "0.005"
-
-    # Собираем параметры для запроса к StaticMapsAPI:
-    map_params = {
-        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-        "spn": ",".join([delta, delta]),
-        "l": "map"
-    }
+def get_map(lon_lat, type, delta="0.005", point=None):
+    if point is None:
+        map_params = {
+            "ll": lon_lat,
+            "spn": ",".join([delta, delta]),
+            "l": type
+        }
+    else:
+        map_params = {
+            "ll": lon_lat,
+            "spn": ",".join([delta, delta]),
+            "l": type,
+            "pt": point
+        }
 
     map_api_server = "http://static-maps.yandex.ru/1.x/"
-    # ... и выполняем запрос
+    return requests.get(map_api_server, params=map_params).content
 
-    response = requests.get(map_api_server, params=map_params)
+
+def save_geo(toponim, map_type, filename):
+    try:
+        lat, lon = get_coords(toponim)
+        lon_lat = f"{lat},{lon}"
+        content = get_map(lon_lat, map_type, point=lon_lat)
+        try:
+            with open(f'static/img/{filename}', 'wb') as file:
+                file.write(content)
+            return filename
+        except IOError as ex:
+            print("Ошибка записи временного файла:", ex)
+            sys.exit(2)
+    except Exception as e:
+        return 'No data' + str(e)
+
+
+def upload_picture(album_id, group_id, filename):
+    login, password = LOGIN, PASSWORD
+    vk_session = vk_api.VkApi(login, password)
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return ''
+
     upload = vk_api.VkUpload(vk_session)
-
-    photo = upload.photo(  # Подставьте свои данные
-        Image.open(BytesIO(response.content)),
+    photo = upload.photo(
+        f'static/img/{filename}',
         album_id=album_id,
         group_id=group_id
     )
-
-    vk_photo_url = 'https://vk.com/photo{}_{}'.format(
-        photo[0]['owner_id'], photo[0]['id']
-    )
-
-    upload_url = vk.photos.getWallUploadServer(group_id=group_id)['upload_url']
-    photo = vk.photos.saveMessagesPhoto(server=upload_url['server'],
-                                        photo=upload_url['photo'],
-                                        hash=upload_url['hash'])
-
-    return f'photo{photo["owner_id"]}_{photo["photo_id"]}'
-
-
+    vk_photo_id = f"photo{photo[0]['owner_id']}_{photo[0]['id']}"
+    return vk_photo_id
 
 
 def main():
-    vk_session = vk_api.VkApi(
-        token=TOKEN)
-
-    longpoll = VkBotLongPoll(vk_session, group_id)
-    n = 0
-    city = ''
+    flag_data, flag_help, flag_type = False, True, False
+    place = ''
     for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            vk = vk_session.get_api()
-            if n == 0:
+        if event.type == VkBotEventType.MESSAGE_NEW and flag_help:
+            flag_data = not flag_data
+            flag_help = not flag_help
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=help(),
+                             random_id=random.randint(0, 2 ** 64))
 
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message="Введите название местности",
-                                 random_id=random.randint(0, 2 ** 64))
-                n += 1
-            elif n == 1:
-                city = event.obj.message['text']
-                vk = vk_session.get_api()
-
-                vk.messages.send(
-                    user_id=event.obj.message['from_id'],
-                    message='Выберите тип карты',
-                    random_id=random.randint(0, 2 ** 64),
-                    keyboard=open('keyboard.json', "r", encoding="UTF-8").read()
-                )
-                n += 1
-            else:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Это {city}. Что вы еще хотите увидеть?",
-                                 random_id=random.randint(0, 2 ** 64),
-                                 keyboard={"buttons": [], "one_time": "true"},
-                                 attachment=geo(city))
-                n = 0
+        elif event.type == VkBotEventType.MESSAGE_NEW and flag_data:
+            flag_data = not flag_data
+            flag_type = not flag_type
+            place = event.obj.message['text']
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=map_type(),
+                             keyboard=open("keyboard.json", "r", encoding="utf-8").read(),
+                             random_id=random.randint(0, 2 ** 64))
+        elif event.type == VkBotEventType.MESSAGE_NEW and flag_type:
+            flag_data = not flag_data
+            flag_type = not flag_type
+            map = event.obj.message['text']
+            album_id = ALBUM_ID
+            pic_name = upload_picture(album_id, -event.obj.message['from_id'], save_geo(place, map, 'map.png'))
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=f'That`s {place}.\n{help()}',
+                             attachment=pic_name,
+                             keyboard=open("keyboard_remove.json", "r", encoding="utf-8").read(),
+                             random_id=random.randint(0, 2 ** 64))
 
 
 if __name__ == '__main__':
     main()
+
